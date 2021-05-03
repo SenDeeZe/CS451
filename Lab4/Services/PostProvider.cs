41629e6919ae53d126185f61d3b322cf987f4b1a
SenDeeZe <64077545+SenDeeZe@users.noreply.github.com>
2021-05-03 12:36:45 +0300

    Lab4

---
+++ Lab4/Services/PostProvider.cs
@@ -0,0 +1,35 @@
+﻿using Lab4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Services
+{
+    public interface IPostProvider
+    {
+        List<PostModel> GetUserPosts(UserModel user);
+    }
+    public class PostProvider: IPostProvider
+    {
+        public List<PostModel> GetUserPosts(UserModel user)
+        {
+            var postWithImage = new Models.PostModel
+            {
+                Author = user,
+                Image = "https://picsum.photos/300/300",
+                Text = "i have a new pic!",
+                Date = DateTime.Now
+            };
+
+            var postWithoutImage = new Models.PostModel
+            {
+                Author = user,
+                Text = "i have no new pics :(",
+                Date = DateTime.Now
+            };
+
+            return new List<PostModel> { postWithoutImage, postWithImage };
+        }
+    }
+}
---
+++ Lab4/Services/UserProvider.cs
@@ -0,0 +1,26 @@
+﻿using Lab4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Services
+{
+    public interface IUserProvider
+    {
+        UserModel GetCurrentUser();
+    };
+    public class UserProvider: IUserProvider
+    {
+        public UserModel GetCurrentUser()
+        {
+            return new UserModel
+            {
+                Name = "Matvei Vasyura",
+                UserName = "Matej",
+                Bio = "hi!",
+                Image = "https://picsum.photos/150/150"
+            };
+        }
+    }
+}
